5b1eb21e2c4772d02670ea9c9c1017e21ddd3b48
AbhinavDholepat <adholepa@uwaterloo.ca>
2020-07-27 12:13:24 -0400

    commit 1

---
+++ Codes/Loading-Data.R
@@ -0,0 +1,11 @@
+install.packages(c("dslabs", "ggplot2", "dplyr", "tidyverse")) 
+library(dslabs)
+library(ggplot2) 
+library(dplyr)
+library(tidyverse)
+
+
+data("brexit_polls")
+view(brexit_polls) 
+
+
